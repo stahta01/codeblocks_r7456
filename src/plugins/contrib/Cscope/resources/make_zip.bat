1e3f6b7f0732d40678c22c977d5ea83768170d7c
Tim S <stahta01@users.sourceforge.net>
2016-06-15 16:29:17 -0400

    * build: Disabled line in batch file.

--- src/plugins/contrib/Cscope/resources/make_zip.bat
+++ src/plugins/contrib/Cscope/resources/make_zip.bat
@@ -2,4 +2,4 @@ cd .\resources
 zip -jq9 ..\..\..\..\build\share\emblocks\cscope.zip manifest.xml *.xrc
 zip -q9  ..\..\..\..\build\share\emblocks\cscope.zip images\*.png
 
-xcopy /I /y ..\Cscope\*.exe ..\..\..\..\build\share\emblocks\Cscope > nul
+REM xcopy /I /y ..\Cscope\*.exe ..\..\..\..\build\share\emblocks\Cscope > nul
