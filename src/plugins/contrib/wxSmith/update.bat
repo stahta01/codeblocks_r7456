0f2764744b63e8d576e6a549e8742c7301f98044
Tim S <stahta01@users.sourceforge.net>
2016-06-15 17:26:34 -0400

    * build: Disabled lines in batch file.

--- src/plugins/contrib/wxSmith/update.bat
+++ src/plugins/contrib/wxSmith/update.bat
@@ -1,9 +1,10 @@
 @echo off
 cls
 zip ..\..\..\build\share\EmBlocks\wxsmith.zip manifest.xml
-zip ..\..\..\output\share\EmBlocks\wxsmith.zip manifest.xml
+REM zip ..\..\..\output\share\EmBlocks\wxsmith.zip manifest.xml
 md ..\..\..\build\share\EmBlocks\images\wxsmith > nul 2>&1
-md ..\..\..\output\share\EmBlocks\images\wxsmith > nul 2>&1
+REM md ..\..\..\output\share\EmBlocks\images\wxsmith > nul 2>&1
 copy wxwidgets\icons\*.png ..\..\..\build\share\EmBlocks\images\wxsmith\ > nul 2>&1
-copy wxwidgets\icons\*.png ..\..\..\output\share\EmBlocks\images\wxsmith\ > nul 2>&1
-exit 0
+REM copy wxwidgets\icons\*.png ..\..\..\output\share\EmBlocks\images\wxsmith\ > nul 2>&1
+exit 0
+
