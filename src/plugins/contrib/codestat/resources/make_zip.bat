9c4555ddecdab34e62511e2d67b96d30564a1cc5
Tim S <stahta01@users.sourceforge.net>
2016-06-17 22:43:15 -0400

    * build: Changed to "build28" from "build" in batch files.

--- src/plugins/contrib/codestat/resources/make_zip.bat
+++ src/plugins/contrib/codestat/resources/make_zip.bat
@@ -1,3 +1,3 @@
 cd .\resources
-zip -jq9 ..\..\..\..\build\share\emblocks\codestat.zip manifest.xml *.xrc
-zip -q9  ..\..\..\..\build\share\emblocks\codestat.zip images\*.png
+zip -jq9 ..\..\..\..\build28\share\emblocks\codestat.zip manifest.xml *.xrc
+zip -q9  ..\..\..\..\build28\share\emblocks\codestat.zip images\*.png
